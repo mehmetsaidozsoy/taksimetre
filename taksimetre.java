9981dcc8c7b9efba793abf539e55587213875191
mehmetsaidozsoy <mehmetsaidozsoy@gmail.com>
2023-05-12 09:12:36 +0300

    basit taksimetre prgramı ekrana yazdırma tamamlandı.

--- taksimetre.java
+++ taksimetre.java
@@ -3,13 +3,18 @@ public class taksimetre{
     public static void main(String[] args) {
         int mesafe=0 , takAc=10 ;
         double ucret=0.0;
-    Scanner input=new Scanner(System.in);
-    System.out.print("Gidilen mesafeyi km olarak giriniz  :");
-    mesafe=input.nextInt();
+    try (Scanner input = new Scanner(System.in)) {
+
+        System.out.print("Gidilen mesafeyi km olarak giriniz  :");
+        if (input.hasNextInt()) {
+            mesafe=input.nextInt();
+        }else{
+            System.out.print("HAtalı giriş yapıldı lütfen bir sayı giriniz.");
+        }
+    }
     ucret=takAc+ (mesafe*2.20);
-    ucret=ucret<20 ? ucret=20: null;
-
-
+    ucret=ucret<20 ? 20.0: ucret;
+    System.out.print("Gidilen mesafeyi tutarı :"+ucret);
 
     }
 }
